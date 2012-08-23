6d6b1305ba8f02ee91f57dff82b37e404b8aa2de
Dave Rupert <rupato@gmail.com>
2012-08-23 18:17:05 -0500

    Quick attempt

---
+++ fitWeird.js
@@ -0,0 +1,29 @@
+;(function(){
+var fillVars = function(){
+  var width = document.getElementById('fitweird-width');
+  var height = document.getElementById('fitweird-height');
+
+  width.innerHTML = window.outerWidth;
+  height.innerHTML = window.outerHeight;
+}
+
+var fitWeird = function(){
+
+  var newDiv = document.createElement('div');
+  var newContent = '<span id=fitweird-width></span> &times; <span id=fitweird-height></span>';
+  newDiv.style.position = 'fixed';
+  newDiv.style.bottom = '0';
+  newDiv.style.right = '0';
+  newDiv.style.backgroundColor = '#3a3a3a';
+  newDiv.style.padding = '0.4em 1em';
+  newDiv.style.color = '#090';
+  newDiv.style.fontFamily = 'monospace';
+  newDiv.innerHTML = newContent;
+  document.body.appendChild(newDiv);
+
+  fillVars();
+  window.addEventListener('resize', 'fillVars');
+};
+
+fitWeird();
+})(window);
