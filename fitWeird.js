2d5fcf907bd93bdfdeef593d304148eae4f94620
Dave Rupert <rupato@gmail.com>
2012-08-23 18:32:28 -0500

    Move to top. Less squirrely.

--- fitWeird.js
+++ fitWeird.js
@@ -12,7 +12,7 @@ var fitWeird = function(){
   var newDiv = document.createElement('div');
   var newContent = '<span id=fitweird-width></span> &times; <span id=fitweird-height></span>';
   newDiv.style.position = 'fixed';
-  newDiv.style.bottom = '0';
+  newDiv.style.top = '0';
   newDiv.style.right = '0';
   newDiv.style.backgroundColor = '#3a3a3a';
   newDiv.style.padding = '0.4em 1em';
