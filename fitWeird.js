cecfe3736fb1cf8a7f51e89dc3d3485cb373e394
Jonas Hermsmeier <jhermsmeier@googlemail.com>
2012-08-24 19:07:22 +0200

    Rewrote fitWeird.js
    - reduced overall size
    - added body base fontsize em calulation
    - improved update performance

--- fitWeird.js
+++ fitWeird.js
@@ -1,55 +1,38 @@
-;(function(){
-
-var widthPx, heightPx, widthEm, heightEm;
-
-var init = function(){
-
-  // Inject the Div
-  setupFitWeird();
-
-  // Gather elements to update
-  widthPx = document.getElementById('fitweird-width-px');
-  heightPx = document.getElementById('fitweird-height-px');
-  widthEm = document.getElementById('fitweird-width-em');
-  heightEm = document.getElementById('fitweird-height-em');
-
-  // Fill the Div
-  fillVars();
-
-  // Update the Div
-  window.addEventListener('resize', fillVars);
-
-}
-
-var fillVars = function(){
-  widthPx.innerHTML = window.innerWidth;
-  heightPx.innerHTML = window.innerHeight;
-
-  // TODO: Calc EM size based off of body font size (i.e user zooming)
-  widthEm.innerHTML = window.innerWidth / 16;
-  heightEm.innerHTML = window.innerHeight / 16;
-
-}
-
-var setupFitWeird = function(){
-
-  if ( !document.getElementById('fitweird-width-px') ) {
-    var newDiv = document.createElement('div');
-    var newContent = '<span id=fitweird-width-px></span>px &times; <span id=fitweird-height-px></span>px ';
-    newContent += ':: <span id=fitweird-width-em></span>em &times; <span id=fitweird-height-em></span>em';
-    newDiv.style.position = 'fixed';
-    newDiv.style.bottom = '0';
-    newDiv.style.right = '0';
-    newDiv.style.backgroundColor = 'rgba(58, 58, 58, 0.8)';
-    newDiv.style.padding = '0.4em 1em';
-    newDiv.style.color = '#00CC00';
-    newDiv.style.fontFamily = 'monospace';
-    newDiv.innerHTML = newContent;
-    document.body.appendChild(newDiv);
+!function( win, doc ){
+  
+  if( !doc.getElementById( 'fitweird' ) ) {
+    
+    var div = doc.createElement( 'div' )
+    var fontSize = parseFloat( getComputedStyle( doc.body ).fontSize )
+    var w, h
+    
+    with( div.style ) {
+      bottom          = 0
+      right           = 0
+      position        = 'fixed'
+      backgroundColor = 'rgba(58, 58, 58, 0.8)'
+      padding         = '0.4em 1em'
+      color           = '#00CC00'
+      fontFamily      = 'monospace'
+    }
+    
+    div.id = 'fitweird'
+    doc.body.appendChild( div )
+    
+    function update() {
+      
+      w = win.innerWidth
+      h = win.innerHeight
+      
+      div.innerHTML =
+        w + 'px &times; ' + h + 'px :: ' +
+        w / fontSize + 'em &times; ' + h / fontSize + 'em'
+      
+    }
+    
+    addEventListener( 'resize', update )
+    update()
+    
   }
-
-};
-
-init();
-
-})(window);
+  
+}( this, document )
