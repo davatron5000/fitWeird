d9359bd71523b8d6c06bb0b6641004fa08247dda
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-11-13 21:35:24 -0500

    modified for rems and included bookmarklet html example

--- fitWeird.js
+++ fitWeird.js
@@ -1,6 +1,6 @@
 ;(function(){
 
-var widthPx, heightPx, widthEm, heightEm;
+var widthPx, heightPx, widthEm, heightEm, widthRem;
 
 var init = function(){
 
@@ -12,6 +12,10 @@ var init = function(){
   heightPx = document.getElementById('fitweird-height-px');
   widthEm = document.getElementById('fitweird-width-em');
   heightEm = document.getElementById('fitweird-height-em');
+  widthRem = document.getElementById('fitweird-width-rem');
+
+  // grab the html element
+  htmlTag = document.getElementsByTagName('html')[0];
 
   // Fill the Div
   fillVars();
@@ -22,27 +26,35 @@ var init = function(){
 }
 
 var fillVars = function(){
+
+  // set the values for the rem unit
+  htmlTag.style.fontSize = '16px';
+  htmlFontSize = htmlTag.style.fontSize,
+  rems = window.innerWidth / parseInt(htmlFontSize);
+
+  // rem value insertion
+  widthRem.innerHTML = rems;
+
   widthPx.innerHTML = window.innerWidth;
   heightPx.innerHTML = window.innerHeight;
 
   // TODO: Calc EM size based off of body font size (i.e user zooming)
   widthEm.innerHTML = window.innerWidth / 16;
   heightEm.innerHTML = window.innerHeight / 16;
-
 }
 
 var setupFitWeird = function(){
 
   if ( !document.getElementById('fitweird-width-px') ) {
     var newDiv = document.createElement('div');
-    var newContent = '<span id=fitweird-width-px></span>px &times; <span id=fitweird-height-px></span>px ';
+    var newContent = '<span id=fitweird-width-rem></span>rem &times; :: <span id=fitweird-width-px></span>px &times; <span id=fitweird-height-px></span>px ';
     newContent += ':: <span id=fitweird-width-em></span>em &times; <span id=fitweird-height-em></span>em';
     newDiv.style.position = 'fixed';
     newDiv.style.bottom = '0';
     newDiv.style.right = '0';
-    newDiv.style.backgroundColor = 'rgba(58, 58, 58, 0.8)';
+    newDiv.style.backgroundColor = 'rgba(220, 28, 28, 0.8)';
     newDiv.style.padding = '0.4em 1em';
-    newDiv.style.color = '#00CC00';
+    newDiv.style.color = '#333';
     newDiv.style.fontFamily = 'monospace';
     newDiv.style.zIndex = '9999';
     newDiv.innerHTML = newContent;
