a8a7993060a440868197d505372773723fc8259f
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-11-14 01:38:08 -0500

    - rem feature - em value for body font-size now computed based on the browsers computed value. Allows for zooming

--- fitWeird.js
+++ fitWeird.js
@@ -1,6 +1,25 @@
 ;(function(){
 
-var widthPx, heightPx, widthEm, heightEm, widthRem;
+// ==================================
+// Defined Global Variables
+// ==================================
+
+var widthPx,
+    heightPx,
+    widthEm,
+    heightEm,
+    widthRem,
+    heightRem,
+    htmlTag,
+    bodyTag,
+    htmlFontSize,
+    bodyFontSize,
+    rems;
+
+
+// ==================================
+// Initialize fitWeird
+// ==================================
 
 var init = function(){
 
@@ -8,59 +27,81 @@ var init = function(){
   setupFitWeird();
 
   // Gather elements to update
-  widthPx = document.getElementById('fitweird-width-px');
-  heightPx = document.getElementById('fitweird-height-px');
-  widthEm = document.getElementById('fitweird-width-em');
-  heightEm = document.getElementById('fitweird-height-em');
-  widthRem = document.getElementById('fitweird-width-rem');
+  widthPx   = document.getElementById('fitweird-width-px');
+  heightPx  = document.getElementById('fitweird-height-px');
+  widthEm   = document.getElementById('fitweird-width-em');
+  heightEm  = document.getElementById('fitweird-height-em');
+  widthRem  = document.getElementById('fitweird-width-rem');
+  heightRem = document.getElementById('fitweird-height-rem');
 
-  // grab the html element
+  // Grab the html element tag
   htmlTag = document.getElementsByTagName('html')[0];
+
+  // Grab the body element tag
+  bodyTag = document.getElementsByTagName('body')[0];
 
   // Fill the Div
   fillVars();
 
   // Update the Div
   window.addEventListener('resize', fillVars);
-
 }
+
+
+// ==================================
+// FillVars Function
+// ==================================
 
 var fillVars = function(){
 
-  // set the values for the rem unit
-  htmlTag.style.fontSize = '16px';
-  htmlFontSize = htmlTag.style.fontSize,
-  rems = window.innerWidth / parseInt(htmlFontSize);
+  // Grab the default computed value of the root element (i.e. html)
+  htmlFontSize = window.getComputedStyle(htmlTag).getPropertyValue('font-size');
+
+  // Grab the body element tags computed font-size value
+  bodyFontSize = window.getComputedStyle(bodyTag).getPropertyValue('font-size');
+
+  // filter the root elements computed value
+  // and divide it by the current window innerWidth
+  remsWidth    = window.innerWidth / parseInt(htmlFontSize);
+  remsHeight   = window.innerHeight / parseInt(htmlFontSize);
 
   // rem value insertion
-  widthRem.innerHTML = rems;
+  widthRem.innerHTML  = remsWidth;
+  heightRem.innerHTML = remsHeight;
 
-  widthPx.innerHTML = window.innerWidth;
-  heightPx.innerHTML = window.innerHeight;
+  // pixel value insertion
+  widthPx.innerHTML   = window.innerWidth;
+  heightPx.innerHTML  = window.innerHeight;
 
-  // TODO: Calc EM size based off of body font size (i.e user zooming)
-  widthEm.innerHTML = window.innerWidth / 16;
-  heightEm.innerHTML = window.innerHeight / 16;
+  // EM size based off of body font size (i.e user zooming)
+  widthEm.innerHTML   = window.innerWidth / parseInt(bodyFontSize);
+  heightEm.innerHTML  = window.innerHeight / parseInt(bodyFontSize);
 }
+
+
+// ==================================
+// fitWeird Setup Call
+// ==================================
 
 var setupFitWeird = function(){
 
   if ( !document.getElementById('fitweird-width-px') ) {
-    var newDiv = document.createElement('div');
-    var newContent = '<span id=fitweird-width-rem></span>rem &times; :: <span id=fitweird-width-px></span>px &times; <span id=fitweird-height-px></span>px ';
-    newContent += ':: <span id=fitweird-width-em></span>em &times; <span id=fitweird-height-em></span>em';
-    newDiv.style.position = 'fixed';
-    newDiv.style.bottom = '0';
-    newDiv.style.right = '0';
-    newDiv.style.backgroundColor = 'rgba(220, 28, 28, 0.8)';
-    newDiv.style.padding = '0.4em 1em';
-    newDiv.style.color = '#333';
-    newDiv.style.fontFamily = 'monospace';
-    newDiv.style.zIndex = '9999';
-    newDiv.innerHTML = newContent;
+    var newDiv                   = document.createElement('div');
+    var newContent               = 'Rems = <span id=fitweird-width-rem></span>rem &times; <span id=fitweird-height-rem></span>rem &#10033; Pixel = <span id=fitweird-width-px></span>px &times; <span id=fitweird-height-px></span>px ';
+    newContent                   += '&#10033; Ems = <span id=fitweird-width-em></span>em &times; <span id=fitweird-height-em></span>em';
+    newDiv.style.position        = 'fixed';
+    newDiv.style.width           = '100%';
+    newDiv.style.bottom          = '0';
+    newDiv.style.right           = '0';
+    newDiv.style.backgroundColor = 'rgba(220, 28, 28, 0.75)';
+    newDiv.style.textAlign       = 'center';
+    newDiv.style.color           = '#333';
+    newDiv.style.fontFamily      = 'monospace';
+    newDiv.style.fontSize        = '1rem';
+    newDiv.style.zIndex          = '9999';
+    newDiv.innerHTML             = newContent;
     document.body.appendChild(newDiv);
   }
-
 };
 
 init();
